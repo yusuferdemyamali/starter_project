94e6a7bd11ad76308ccea1f03773c31a78a522c8
yusuferdem2 <132130033@ogr.uludag.edu.tr>
2025-08-11 19:38:40 +0300

    Faq modeli için HasFactory kullanımı eklendi ve doldurulabilir alanlar tanımlandı.

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Faq extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'question',
+        'answer',
+        'is_active',
+        'order',
+    ];
 }
