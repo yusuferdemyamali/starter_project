d5ab0154cd4e3576949c10e96370b55078bd80a9
Yusuf Erdem Yamalı <yusuferdem.dev@gmail.com>
2025-08-12 18:01:33 +0300

    SiteSettingResource ve ilgili sayfalar için temel yapı eklendi.

---
+++ app/Filament/Resources/SiteSettingResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SiteSettingResource\Pages;
+use App\Filament\Resources\SiteSettingResource\RelationManagers;
+use App\Models\SiteSetting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SiteSettingResource extends Resource
+{
+    protected static ?string $model = SiteSetting::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSiteSettings::route('/'),
+            'create' => Pages\CreateSiteSetting::route('/create'),
+            'edit' => Pages\EditSiteSetting::route('/{record}/edit'),
+        ];
+    }
+}
