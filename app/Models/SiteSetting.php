2d2e5c8dc80ca5458150af3bf8e923162bb4ae21
Yusuf Erdem Yamalı <yusuferdem.dev@gmail.com>
2025-08-12 17:50:38 +0300

    SiteSetting modeline $fillable özelliği eklendi.

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteSetting extends Model
 {
-    //
+    protected $fillable = [
+        'site_name',
+        'site_email',
+        'site_phone',
+        'site_address',
+        'site_logo',
+        'site_favicon',
+        'site_facebook_url',
+        'site_twitter_url',
+        'site_linkedin_url',
+        'site_instagram_url',
+        'site_youtube_url',
+        'site_working_hours',
+        'site_maps_embed',
+        'site_seo_title',
+        'site_seo_description',
+        'site_seo_keywords',
+    ];
 }
