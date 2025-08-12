7fba1585b57d27fb89c669789651ee5058ae62d0
Yusuf Erdem Yamalı <yusuferdem.dev@gmail.com>
2025-08-12 17:54:35 +0300

    SiteSetting migration dosyası dolduruldu. migrate edildi.

--- database/migrations/2025_08_12_144700_create_site_settings_table.php
+++ database/migrations/2025_08_12_144700_create_site_settings_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('site_settings', function (Blueprint $table) {
             $table->id();
+            $table->string('site_name', 100);
+            $table->string('site_email')->nullable();
+            $table->string('site_phone')->nullable();
+            $table->string('site_address')->nullable();
+            $table->string('site_logo')->nullable();
+            $table->string('site_favicon')->nullable();
+            $table->string('site_facebook_url')->nullable();
+            $table->string('site_twitter_url')->nullable();
+            $table->string('site_linkedin_url')->nullable();
+            $table->string('site_instagram_url')->nullable();
+            $table->string('site_youtube_url')->nullable();
+            $table->text('site_working_hours')->nullable();
+            $table->text('site_maps_embed')->nullable();
+            $table->string('site_seo_title')->nullable();
+            $table->text('site_seo_description')->nullable();
+            $table->text('site_seo_keywords')->nullable();
             $table->timestamps();
         });
     }
